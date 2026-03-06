731aac4e4f341d28991cb5663e9c95ac30fc6e8c
Ethan Bryce Lopeña <ethan.yce@gmail.com>
2026-03-07 01:56:47 +0800

    Merged Login with Customer and Provider Registration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-<<<<<<< HEAD
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
-}
-bootstrap();
-=======
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
@@ -22,4 +14,3 @@ async function bootstrap() {
   console.log('Server is running on http://localhost:5000');
 }
 bootstrap();
->>>>>>> origin/customer-registration
