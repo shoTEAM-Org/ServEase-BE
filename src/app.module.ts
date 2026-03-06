b16bbf698c7abda33b57be49961ae825eff500cb
ethanbrycelopena <ethanbryce.lopena.cics@ust.edu.ph>
2026-03-06 23:30:54 +0800

    Login Feature Integrated into Main

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule} from '@nestjs/config';
+import { UsersModule } from './modules/users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  UsersModule,
+],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/modules/users/dto/login-user.dto.ts
@@ -0,0 +1,12 @@
+import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+
+
+export class LoginUserDto {
+   @IsEmail()
+   @IsNotEmpty()
+   email: string;
+
+   @IsString()
+   @IsNotEmpty()
+   password: string; 
+}
