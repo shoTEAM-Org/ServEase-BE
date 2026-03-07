12ce04e41a279877d23febb9d9fc8c7b56de3dab
Marukom0808 <malcolm.petines884@gmail.com>
2026-03-07 20:45:52 +0800

    feat: implement service search and trust score sorting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module'; 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServicesModule } from './modules/services/services.module';
 
 @Module({
   imports: [
     AuthModule,
+    ServicesModule
   ],
   controllers: [AppController],
   providers: [AppService],
